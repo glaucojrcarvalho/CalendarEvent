04e77a5802c415d4af650d87281bb3c5eace3692
glaucojrcarvalho <glaucojrcarvalho@gmail.com>
2024-02-20 16:23:20 -0300

    closes #10; feat(EventRecurringService): add feature event recurring service in event domain

---
+++ src/app/Domain/Services/EventRecurringService.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Domain\Services;
+
+use App\Domain\Entities\Event;
+use Carbon\Carbon;
+
+class EventRecurringService
+{
+    public function repeat($data): array
+    {
+        return $this->createRecurringEvents($data);
+    }
+    public function getNextOccurrence($data): array
+    {
+        $nextOccurrenceStart = null;
+        $nextOccurrenceEnd = null;
+
+        if($data['frequency'] == 'daily') {
+            $nextOccurrenceStart = Carbon::create($data['startDate'])->addDay()->format('Y-m-d H:i:s');
+            $nextOccurrenceEnd = Carbon::create($data['endDate'])->addDay()->format('Y-m-d H:i:s');
+        } else if($data['frequency'] == 'weekly') {
+            $nextOccurrenceStart = Carbon::create($data['startDate'])->addWeek()->format('Y-m-d H:i:s');
+            $nextOccurrenceEnd = Carbon::create($data['endDate'])->addWeek()->format('Y-m-d H:i:s');
+        } else if($data['frequency'] == 'monthly') {
+            $nextOccurrenceStart = Carbon::create($data['startDate'])->addMonth()->format('Y-m-d H:i:s');
+            $nextOccurrenceEnd = Carbon::create($data['endDate'])->addMonth()->format('Y-m-d H:i:s');
+        } else if($data['frequency'] == 'yearly') {
+            $nextOccurrenceStart = Carbon::create($data['startDate'])->addYear()->format('Y-m-d H:i:s');
+            $nextOccurrenceEnd = Carbon::create($data['endDate'])->addYear()->format('Y-m-d H:i:s');
+        }
+
+        return [$nextOccurrenceStart, $nextOccurrenceEnd];
+    }
+    public function createRecurringEvents($data)
+    {
+        $recurringEvents = [];
+        $nextOccurrence = $this->getNextOccurrence($data);
+
+        while($nextOccurrence[0] <= $data['repeatUntil']) {
+            $recurringEvent = new Event([
+                'title' => $data['title'],
+                'description' => $data['description'],
+                'startDate' => $nextOccurrence[0],
+                'endDate' => $nextOccurrence[1],
+                'recurringPattern' => $data['recurringPattern']
+            ]);
+
+            array_push($recurringEvents, $recurringEvent);
+            $data['startDate'] = $nextOccurrence[0];
+            $data['endDate'] = $nextOccurrence[1];
+            $nextOccurrence = $this->getNextOccurrence($data);
+        }
+
+        return $recurringEvents;
+    }
+}
